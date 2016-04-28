587d88401eb0e1852e02d140a9d49104bb94ce6e
Matt <matt.tomlinson1@gmail.com>
2016-04-28 00:20:15 -0600

    Tab labels added

--- app/src/main/java/app/myapplication/MainActivity.java
+++ app/src/main/java/app/myapplication/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_3));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
