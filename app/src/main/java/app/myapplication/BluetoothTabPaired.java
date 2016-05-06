65ffd13c9dc83ccfeac36838bd50a67abbdd5061
mavs0890 <marlon.alejandro.vilorio@gmail.com>
2016-05-06 16:46:13 -0600

    Adding all bluetooth activities and tabs

---
+++ app/src/main/java/app/myapplication/BluetoothTabPaired.java
@@ -0,0 +1,18 @@
+package app.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by marlonvilorio on 5/5/16.
+ */
+public class BluetoothTabPaired extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
