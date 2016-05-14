0a904c234d080d14830afd502dd65275df821d88
alexsmu <alexsmu@gmail.com>
2016-05-13 18:12:14 -0600

    Cleaned up the code a bit to match the modified Fft.java class file

--- app/src/main/java/app/myapplication/TabFragment5.java
+++ app/src/main/java/app/myapplication/TabFragment5.java
@@ -15,37 +15,38 @@ import com.jjoe64.graphview.series.Series;
 
 public class TabFragment5 extends Fragment {
     private View mMain = null;
-    private Fft myFft = new Fft();
+    private int n = 8192;
+    private float Fs = 44100;
+    private Fft myFft = new Fft(n, Fs);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMain = inflater.inflate(R.layout.tab_fragment_5, container, false);
-        int n = 8192;
-        double Fs = 44100;
-        double f = 200;
-        double[] y_real = new double[n];
-        double[] y_imag = new double[n];
-        double[] Y_mag = new double[n];
-        double[] Y_shifted = new double[n];
-        double[] omega = new double[n];
+
+        float f = 200;
+        float[] y_real = new float[n];
+        float[] y_imag = new float[n];
+        float[] Y_mag;
+        float[] Y_shifted;
+        float[] omega;
 
         GraphView graph = (GraphView) mMain.findViewById(R.id.graph);
         DataPoint[] dps = new DataPoint[n];
         for (int i = 0; i < n; ++i) {
-            y_real[i] = 2 * Math.cos(2 * Math.PI * f * i / Fs);
+            y_real[i] = 2 * (float)Math.cos(2 * Math.PI * f * i / Fs);
             y_imag[i] = 0;
         }
 
         myFft.transform(y_real, y_imag);
-        myFft.getMagnitudeDB(y_real, y_imag, Y_mag);
-        myFft.shift(Y_mag, Y_shifted);
-        myFft.getOmega(omega);
+        Y_mag = myFft.getMagnitudeDB(y_real, y_imag);
+        Y_shifted = myFft.shift(Y_mag);
+        omega = myFft.getOmega();
 
         for (int i = 0; i < n; ++i){
             dps[i] = new DataPoint(omega[i], Y_shifted[i]);
         }
 
-        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<DataPoint>(dps);
+        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(dps);
         series.setOnDataPointTapListener(new OnDataPointTapListener() {
             @Override
             public void onTap(Series series, DataPointInterface dataPoint) {
