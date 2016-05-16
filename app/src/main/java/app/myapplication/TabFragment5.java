72505181fca145ac2ce57b9cb2c9bebd6230bacb
alexsmu <alexsmu@gmail.com>
2016-05-15 18:30:12 -0600

    Restored double datatype for precision, and in order to keep app compatible with API 15 (float return type for audioRecord is API 23) Restructured code so part of it will run when user switches to the tab instead of onCreateView.

--- app/src/main/java/app/myapplication/TabFragment5.java
+++ app/src/main/java/app/myapplication/TabFragment5.java
@@ -16,48 +16,62 @@ import com.jjoe64.graphview.series.Series;
 public class TabFragment5 extends Fragment {
     private View mMain = null;
     private int n = 8192;
-    private float Fs = 44100;
+    private double Fs = 44100;
     private Fft myFft = new Fft(n, Fs);
+    private GraphView graph = null;
+    private PointsGraphSeries<DataPoint> series = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMain = inflater.inflate(R.layout.tab_fragment_5, container, false);
-
-        float f = 200;
-        float[] y_real = new float[n];
-        float[] y_imag = new float[n];
-        float[] Y_mag;
-        float[] Y_shifted;
-        float[] omega;
-
-        GraphView graph = (GraphView) mMain.findViewById(R.id.graph);
-        DataPoint[] dps = new DataPoint[n];
-        for (int i = 0; i < n; ++i) {
-            y_real[i] = 2 * (float)Math.cos(2 * Math.PI * f * i / Fs);
-            y_imag[i] = 0;
-        }
-
-        myFft.transform(y_real, y_imag);
-        Y_mag = myFft.getMagnitudeDB(y_real, y_imag);
-        Y_shifted = myFft.shift(Y_mag);
-        omega = myFft.getOmega();
-
-        for (int i = 0; i < n; ++i){
-            dps[i] = new DataPoint(omega[i], Y_shifted[i]);
-        }
-
-        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(dps);
+        series = new PointsGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 0)});
         series.setOnDataPointTapListener(new OnDataPointTapListener() {
             @Override
             public void onTap(Series series, DataPointInterface dataPoint) {
-                Toast.makeText(mMain.getContext(), "Data Point: "+dataPoint, Toast.LENGTH_SHORT).show();
+                Toast.makeText(mMain.getContext(), "Data Point: " + dataPoint, Toast.LENGTH_SHORT).show();
             }
         });
+        graph = (GraphView) mMain.findViewById(R.id.math_graph);
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(-400);
         graph.getViewport().setMaxX(400);
         graph.addSeries(series);
-
+        if (getUserVisibleHint());
+            setUserVisibleHint(true);
         return mMain;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser){
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser && mMain != null) {
+            double f = 200;
+            double[] y_real = new double[n];
+            double[] y_imag = new double[n];
+            double[] Y_mag;
+            double[] Y_shifted;
+            double[] omega;
+
+            DataPoint[] dps = new DataPoint[n];
+            for (int i = 0; i < n; ++i) {
+                y_real[i] = 2 * Math.cos(2 * Math.PI * f * i / Fs);
+                y_imag[i] = 0;
+            }
+
+            myFft.transform(y_real, y_imag);
+            Y_mag = myFft.getMagnitudeDB(y_real, y_imag);
+            Y_shifted = myFft.shift(Y_mag);
+            omega = myFft.getOmega();
+
+            for (int i = 0; i < n; ++i) {
+                dps[i] = new DataPoint(omega[i], Y_shifted[i]);
+            }
+            series.resetData(dps);
+        }
+        else {
+            if (series != null)
+                series.resetData(new DataPoint[]{new DataPoint(0, 0)});
+        }
     }
 }
